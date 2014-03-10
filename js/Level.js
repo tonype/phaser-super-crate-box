4c1d50767d6c7812bb7f6b43cae8af9c25a15f48
Tony Perriello <tony.perriello@gmail.com>
2014-03-10 19:39:14 -0400

    Moved Level stuff to Level.js

--- js/Level.js
+++ js/Level.js
@@ -9,14 +9,27 @@ Level = function(game, player) {
 Level.prototype = {
  
     preload: function() {
-
+		this.game.load.tilemap('scb', 'assets/scb.json', null, Phaser.Tilemap.TILED_JSON);
+		this.game.load.image('sky', 'assets/sky.png');
+		this.game.load.image('ground', 'assets/platform.png');
+		this.game.load.image('tile', 'assets/tile.png');
     },
  
     create: function() {
- 
+		// add background for this level
+		this.game.add.sprite(0, 0, 'sky');
+
+		this.map = game.add.tilemap('scb');
+		
+		this.map.addTilesetImage('scb_level', 'tile');
+		this.map.setCollision(1, true);
+		
+		this.layer = this.map.createLayer('layer1');
+		this.layer.resizeWorld();
     },
  
     update: function() {
+		// nothing yet
     }
  
 };
